f85ae9240f8cb675b05dc4d9b0d5bd149c8610cc
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-01 15:18:18 -0700

    Adds logic for finding the correct route for request

--- skeleton/lib/router.rb
+++ skeleton/lib/router.rb
@@ -10,9 +10,9 @@ class Route
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (http_method == req.request_method.downcase.to_sym) && !!(pattern =~ req.path)
     # req.request_method.downcase == @http_method
     # req.path =~ @pattern
-    (http_method == req.request_method.downcase.to_sym) && !!(pattern =~ req.path)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -26,10 +26,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -40,10 +42,15 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
+
 
   # should return the route that matches this request
   def match(req)
+    @routes.find {|route| route.matches?(req)}
   end
 
   # either throw 404 or call run on a matched route
